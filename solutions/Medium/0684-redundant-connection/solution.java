0954542c957cbf66fde7cf6b716e1ae1aa272a8f
Rajasudhan-17 <rajasudhan17@users.noreply.github.com>
2026-08-28 06:23:54 +0000

    leetie: Add 684. Redundant Connection [Medium] (java)

--- solutions/Medium/0684-redundant-connection/solution.java
+++ solutions/Medium/0684-redundant-connection/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Depth-First Search, Breadth-First Search, Union-Find, Graph Theory
 // Link     : https://leetcode.com/problems/redundant-connection/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42908000 (beats 0%)
+// Runtime  : 3 ms (beats 0%)
+// Memory   : 42636000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 Rajasudhan-17. All rights reserved.
 // Synced by: leetie
@@ -12,25 +12,32 @@
 
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        int n=edges.length;
-        int[] parent=new int[edges.length];
+        int n=edges.length+1;
+        int[] parent=new int[edges.length+1];
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int rootA=find(i,parent);
-                int rootB=find(j,parent);
-                if(rootA==rootB) return new int[] {i,j};
-            }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         int rootA=find(edges[i],parent);
+        //         int rootB=find(edges[j],parent);
+        //         if(rootA==rootB) return new int[] {i,j};
+        //     }
+        // }
+        for(int[] ele:edges){
+            int rootA=find(ele[0],parent);
+            int rootB=find(ele[1],parent);
+            System.out.println(rootA+ " " + rootB);
+            System.out.println(Arrays.toString(parent));
+            if(rootA==rootB) return new int[] {ele[0],ele[1]};
+            parent[ele[1]] = ele[0];
         }
         return new int[] {};
     }
     public static int find(int x,int[] parent){
-        if(parent[x]==x){
-            return x;
+        if(parent[x]!=x){
+            parent[x]=find(parent[x],parent);
         }
-        parent[x]=find(parent[x],parent);
         return parent[x];
     }
 
